a0296b289e581c38d99ee3a692fab6672099f6f9
stephensmith771 <stephen.smith771@protonmail.com>
2025-12-12 22:22:24 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "to Install, Download, and Reinstall Quicken on a New Computer: Step-by-Step Guide via Quicken.com"
+html_title = "Install Quicken on This Computer: Complete Guide to Quicken.com Download for a New Windows 11 Device"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
